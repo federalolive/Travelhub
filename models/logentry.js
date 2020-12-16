d6b62eccf7101f258698c64655d1b2873b046bc0
federalolive <j3pumphrey@gmail.com>
2020-12-15 22:03:55 -0600

    Create log entry schema

--- models/logentry.js
+++ models/logentry.js
@@ -1,13 +1,45 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-requiredString = {
-    type: {String, 
-    required: true,
+const requiredString = {
+  type: String,
+  required: true,
 };
 
-const LogEntrySchema = new Schema ({
-    title: requiredString,
-})
+const LogEntrySchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    comments: String,
+    rating: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    latitude: {
+      ...requiredNumber,
+      min: -90,
+      max: 90,
+    },
+    longitude: {
+      ...requiredNumber,
+      min: -180,
+      max: 180,
+    },
+    visitDate: {
+      required: true,
+      type: Date,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-module.exports = mongoose.model('LogEntry', LogEntrySchema)
+const LogEntry = mongoose.model("LogEntry", logEntrySchema);
+
+module.exports = LogEntry;
