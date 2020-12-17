9a2d46ae2bc5c92996c946b7fda487a28b494c2d
federalolive <j3pumphrey@gmail.com>
2020-12-17 10:59:45 -0600

    Create index function

--- controllers/logs.js
+++ controllers/logs.js
@@ -2,8 +2,19 @@ const Game = require('../models/game')
 const axios = require('axios')
 
 module.exports = {
-    
+    index,
 }
+
+function index(req, res) {
+    Log.find({ favoritedBy: req.user._id })
+    .then((games) => {
+      res.render('logs/index', {
+        title: "Travel Log",
+        user: req.user,
+        games
+      })
+    })
+  }
 
 
 
