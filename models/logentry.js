fcf4dd528f46d545ee199d32e5d0ddd4409e0636
federalolive <j3pumphrey@gmail.com>
2020-12-17 10:48:30 -0600

    Add logs router, make various changes to app structure

--- models/logentry.js
+++ models/logentry.js
@@ -1,45 +1,43 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
 
-const requiredString = {
-  type: String,
+const requiredNumber = {
+  type: Number,
   required: true,
 };
 
-const LogEntrySchema = new Schema(
-  {
-    title: {
-      type: String,
-      required: true,
-    },
-    description: String,
-    comments: String,
-    rating: {
-      type: Number,
-      min: 0,
-      max: 10,
-      default: 0,
-    },
-    latitude: {
-      ...requiredNumber,
-      min: -90,
-      max: 90,
-    },
-    longitude: {
-      ...requiredNumber,
-      min: -180,
-      max: 180,
-    },
-    visitDate: {
-      required: true,
-      type: Date,
-    },
+const logEntrySchema = new Schema({
+  title: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
-  }
-);
+  description: String,
+  comments: String,
+  image: String,
+  rating: {
+    type: Number,
+    min: 0,
+    max: 10,
+    default: 0,
+  },
+  latitude: {
+    ...requiredNumber,
+    min: -90,
+    max: 90,
+  },
+  longitude: {
+    ...requiredNumber,
+    min: -180,
+    max: 180,
+  },
+  visitDate: {
+    required: true,
+    type: Date,
+  },
+}, {
+  timestamps: true,
+});
 
-const LogEntry = mongoose.model("LogEntry", logEntrySchema);
+const LogEntry = mongoose.model('LogEntry', logEntrySchema);
 
 module.exports = LogEntry;
