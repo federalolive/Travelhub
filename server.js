e478a6107cc5d97f6e5f52d017bd89466834ba77
federalolive <j3pumphrey@gmail.com>
2020-12-17 18:44:28 -0600

    Add map api code to index.ejs

--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const logger = require("morgan");
 const session = require("express-session");
 const passport = require("passport");
 const methodOverride = require("method-override");
+var mapboxgl = require('mapbox-gl/dist/mapbox-gl.js');
 
 // load env variables
 require("dotenv").config();
