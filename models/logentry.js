e998238f2d7f07824f450c713dd8b2101a101d43
federalolive <j3pumphrey@gmail.com>
2020-12-19 12:25:22 -0600

    Create logentryschema.

--- models/logentry.js
+++ models/logentry.js
@@ -1,4 +1,26 @@
 const mongoose = require('mongoose')
 const Schema = mongoose.Schema
 
-module.exports = mongoose.model('')
+const logEntrySchema = new Schema({
+    title: {
+      type: String,
+      required: true,
+    },
+    description: String,
+    comments: String,
+    image: String,
+    rating: {
+      type: Number,
+      min: 0,
+      max: 10,
+      default: 0,
+    },
+    visitDate: {
+      required: true,
+      type: Date,
+    },
+  }, {
+    timestamps: true,
+  });
+
+module.exports = mongoose.model('LogEntry', logEntrySchema)
