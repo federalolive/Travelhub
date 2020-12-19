b5096a171d8d4d3ad8015efcb73142870a0ba3f6
federalolive <j3pumphrey@gmail.com>
2020-12-18 23:56:40 -0600

    Add getplace module.export.

--- controllers/places.js
+++ controllers/places.js
@@ -3,7 +3,7 @@ const axios = require('axios')
 
 module.exports = {
     index,
-    
+    getPlace,
     addPlace,
     
 }
@@ -18,6 +18,21 @@ function index(req, res) {
       })
     })
   }
+
+  async function getPlace(req, res, next) {
+    try {
+      const place = await Place.create(req.body);
+
+      return res.status(200).json({
+          success: true,
+          data: place
+      });
+  } catch (err) {
+      console.log(err);
+      res.status(500);
+  }
+};
+
 
 async function addPlace(req, res, next) {
   try {
