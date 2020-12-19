1ed1cf7d81699152f0096e1619ad5256a0647351
federalolive <j3pumphrey@gmail.com>
2020-12-18 23:28:22 -0600

    Create new PlaceSchema.

---
+++ models/place.js
@@ -0,0 +1,15 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const PlaceSchema = new mongoose.Schema({
+  address: {
+      type: String,
+      required: [true, 'Please add an address']
+  },
+  createdAt: {
+      type: Date,
+      default: Date.now()
+  }
+});
+
+module.exports = mongoose.model('place', placeSchema)
