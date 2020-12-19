89e95de53fb2f740916fda68174c83e0691ab344
federalolive <j3pumphrey@gmail.com>
2020-12-18 20:00:59 -0600

    Edit maps route

--- routes/maps.js
+++ routes/maps.js
@@ -1,8 +1,11 @@
 const router = require("express").Router();
 const mapsCtrl = require('../controllers/maps')
+const logsCtrl = require('../controllers/logs')
 
 router.get('/index', isLoggedIn, mapsCtrl.index)
 router.get('/', isLoggedIn, mapsCtrl.new)
+router.get('/maps', isLoggedIn, mapsCtrl.new)
+router.post('/logs', isLoggedIn, logsCtrl.create)
 
 function isLoggedIn(req, res, next) {
     if (req.isAuthenticated()) return next();
