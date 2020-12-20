636b59d3e75547eb918ba1dab3fd63bdc1e4706f
federalolive <j3pumphrey@gmail.com>
2020-12-20 17:15:12 -0600

    touch comments controllers, create create function.

---
+++ controllers/comments.js
@@ -0,0 +1,16 @@
+const Log = require('../models/log')
+
+module.exports = {
+  create
+}
+
+function create(req, res) {
+  Log.findById(req.params.id)
+  .then((log) => {
+    log.comments.push(req.body)
+    log.save()
+    .then(() => {
+      res.redirect(`/logs/${log.slug}`)
+    })
+  })
+}
